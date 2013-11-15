ef4655e06987d9be87f7277ae70ceb04f3060a0d
Andrey Smirnov <s.andrey.d@gmail.com>
2013-11-16 03:10:02 +0400

    Парсинг понедельной статистики ПАММ-счета. Методы-заглушки под сохранение в БД.

--- classes/Pamm.php
+++ classes/Pamm.php
@@ -6,14 +6,32 @@
  */
 class Pamm
 {
+	/**
+	 * Название ПАММ-счета на сайте брокера.
+	 *
+	 * @var string
+	 */
 	private $name;
+
+	/**
+	 * ID ПАММ-счета на сайте брокера.
+	 *
+	 * @var int
+	 */
 	private $vendorId;
+
+	/**
+	 * Дата открытия ПАММ-счета.
+	 *
+	 * @var string
+	 */
 	private $startedAt;
 
 	/**
 	 * Создает ПАММ-счет с базовой информацией из строки таблицы рейтинга.
 	 *
 	 * @param DOMElement $row
+	 * @return $this
 	 */
 	public static function fromRatingTable(DOMElement $row)
 	{
@@ -27,41 +45,100 @@ class Pamm
 	}
 
 	/**
-	 * Заходит на страницу конкретного ПАММ-счета, используя ID, и получает всю дополнительную информацию.
+	 * Получает информацию о текущем счете из БД, если она там есть.
+	 *
+	 * @return bool
 	 */
 	public function getSpecifics()
 	{
-		echo "Получаем специфичную информацию по счету {$this->getName()} ({$this->getVendorId()})\n";
+		//echo "Получаем специфичную информацию по счету {$this->getName()} ({$this->getVendorId()})\n";
+		return false;
+	}
+
+	/**
+	 * Добавляет запись о статистике ПАММ-счета за переданную неделю.
+	 * Если такая запись уже есть в БД, дубликат не делается.
+	 *
+	 * @param int $weekNumber ex: 42
+	 * @param int $year       ex: 2013
+	 * @param float $percent  ex: 2.14
+	 */
+	public function addWeekStats($weekNumber, $year, $percent)
+	{
+
+	}
+
+	/**
+	 * Сохраняет ПАММ-счет в базе данных.
+	 *
+	 * @return void
+	 */
+	public function save()
+	{
+		echo "Сохраняем информацию по счету {$this->getName()} ({$this->getVendorId()})\n";
 	}
 
 
+	/**
+	 * Устанавливает имя ПАММ-счета.
+	 *
+	 * @param string $name
+	 * @return $this
+	 */
 	public function setName($name)
 	{
 		$this->name = trim($name);
 		return $this;
 	}
 
+	/**
+	 * Устанавливает внешний ID ПАММ-счета.
+	 *
+	 * @param int $vendorId
+	 * @return $this
+	 */
 	public function setVendorId($vendorId)
 	{
-		$this->vendorId = trim($vendorId);
+		$this->vendorId = intval(trim($vendorId));
 		return $this;
 	}
 
+	/**
+	 * Устанавливает дату открытия ПАММ-счета
+	 *
+	 * @param string $startedAt
+	 * @return $this
+	 */
 	public function setStartedAt($startedAt) {
 		$this->startedAt = trim($startedAt);
 		return $this;
 	}
 
+	/**
+	 * Возвращает название ПАММ-счета.
+	 *
+	 * @return string
+	 */
 	public function getName()
 	{
 		return $this->name;
 	}
 
+	/**
+	 * Возвращает внешний ID ПАММ-счета.
+	 *
+	 * @return int
+	 */
 	public function getVendorId()
 	{
 		return $this->vendorId;
 	}
 
+	/**
+	 * Возвращает дату открытия ПАММ-счета.
+	 *
+	 * @return string
+	 */
 	public function getStartedAt()
 	{
 		return $this->startedAt;
--- classes/PammRating.php
+++ classes/PammRating.php
@@ -14,19 +14,33 @@ class PammRating
 	const CSS_SELECTOR = "table.my_accounts_table[1] tr";
 
 	/**
-	 * Заходит на страницу рейтинга ПАММ-счетов и получает всю информацию о них.
+	 * Инкапсулирует работу с Zend\Http\Client. Возвращает сразу HTML код переданного Uri.
+	 *
+	 * @param string $uri
+	 * @return string HTML код страницы.
 	 */
-	public function getAllPamms()
+	private function requestUri($uri)
 	{
 		$request = new Request();
-		$request->setUri(self::URI_RATING);
+		$request->setUri($uri);
 
 		$client = new Client();
 		$response = $client->send($request);
 
-		$body = $response->getBody();
+		return $response->getBody();
+	}
 
-		$dom = new Query($body);
+	/**
+	 * Заходит на страницу рейтинга ПАММ-счетов и получает список DOMElement'ов с ПАММ-счетами.
+	 *
+	 * @return DOMElement[]
+	 */
+	private function getAllPammsRaw()
+	{
+		$pamms = [];
+
+		$html = $this->requestUri(self::URI_RATING);
+		$dom = new Query($html);
 		$elements = $dom->execute(self::CSS_SELECTOR);
 
 		$needToParse = false;
@@ -55,8 +69,74 @@ class PammRating
 				continue;
 			}
 
+			$pamms[] = $element;
+		}
+
+		return $pamms;
+	}
+
+	/**
+	 * Заходит на страницу рейтинга ПАММ-счетов и получает всю информацию о них.
+	 */
+	public function getAllPamms()
+	{
+		$elements = $this->getAllPammsRaw();
+		foreach ($elements as $element) {
 			$pamm = Pamm::fromRatingTable($element);
-			$pamm->getSpecifics();
+			$this->getSpecifics($pamm);
+			$pamm->save();
 		}
 	}
+
+	/**
+	 * Заходит на страницу конкретного ПАММ-счета, используя ID, и получает всю дополнительную информацию.
+	 *
+	 * @param Pamm $pamm
+	 */
+	public function getSpecifics(Pamm $pamm)
+	{
+		// Если информация уже есть в БД, просто возвращаем её.
+		if ($pamm->getSpecifics()) {
+			return;
+		}
+
+		$uri = "http://fx-trend.com/pamm/{$pamm->getVendorId()}/";
+		$html = $this->requestUri($uri);
+
+		$dom = new Query($html);
+		$elements = $dom->execute('table.my_accounts_table tr');
+
+
+		$needToParse = false;
+		foreach ($elements as $element) {
+			if (!$element->nodeValue) {
+				continue;
+			}
+
+			// Как только мы находим заголовок таблицы - начинаем парсить понедельную статистику.
+			if (substr($element->nodeValue, 0, 12) === 'Неделя') {
+				$needToParse = true;
+				continue;
+			}
+
+			if (!$needToParse) {
+				continue;
+			}
+
+			$info = explode("\t\t\t\t\t", $element->nodeValue);
+
+			// Преобразовываем диапазон вида 11.11.2013 - 17.11.2013 в порядковый номер недели
+			$mondayDate      = substr($info[0], 0, 10);
+			$mondayTimestamp = strtotime($mondayDate);
+
+			$weekNumber = date('W', $mondayTimestamp);
+			$year       = date('Y', $mondayTimestamp);
+			$percent = rtrim($info[2], "%\t\n");
+
+			$pamm->addWeekStats($weekNumber, $year, $percent);
+
+		}
+	}
+
+
 }
